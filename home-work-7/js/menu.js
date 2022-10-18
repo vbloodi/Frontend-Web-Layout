4da3b95afbaee9e88fd8f69047a5e43d91c21361
vbloodi <bloodi.code@gmail.com>
2022-10-18 15:17:50 +0300

    hw7 burger-menu added

--- home-work-7/js/menu.js
+++ home-work-7/js/menu.js
@@ -1,6 +1,11 @@
 window.addEventListener("DOMContentLoaded", function () {
+  document.querySelector("#burger-open").addEventListener("click", function () {
+    document.querySelector("#burger-menu").classList.toggle("is-active");
+  });
+});
+window.addEventListener("DOMContentLoaded", function () {
   document
-    .querySelector("#burger-button")
+    .querySelector("#burger-close")
     .addEventListener("click", function () {
       document.querySelector("#burger-menu").classList.toggle("is-active");
     });
